700627afc4064448f6e3c6a35d043234c4297383
derickfelix <derickfelix4321@gmail.com>
2017-12-30 10:22:28 -0500

    Create models

---
+++ src/main/java/com/bookstore/models/User.java
@@ -0,0 +1,50 @@
+/** Open Source code **/
+package com.bookstore.models;
+
+/**
+ *
+ * @author derickfelix
+ * @Date 12/30/17
+ */
+public class User {
+    
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
