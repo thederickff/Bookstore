8f879b3f70b143b55a402e0654c67a52bb8d9089
Derick Felix <derickfelix4321@gmail.com>
2018-01-02 16:02:54 -0300

    Doc(): Add javadoc to UserRepository

--- src/main/java/com/bookstore/repositories/UserRepository.java
+++ src/main/java/com/bookstore/repositories/UserRepository.java
@@ -1,4 +1,4 @@
-/** Open Source code **/
+/** Open Source code * */
 package com.bookstore.repositories;
 
 import com.bookstore.models.User;
@@ -10,12 +10,34 @@ import java.util.List;
  * @Date 12/30/17
  */
 public interface UserRepository {
-    
+
+    /**
+     * Gets all the users saved in the storage
+     *
+     * @return a List of users
+     */
     public List<User> all();
-    
+
+    /**
+     * Add a new user to the storage
+     *
+     * @param user the user bean to be added
+     */
     public void add(User user);
-    
+
+    /**
+     * Finds a user by specifying its id
+     *
+     * @param id the id of the user
+     * @return the user that matches with the id specified
+     */
     public User find(int id);
-    
+
+    /**
+     * Delete a user
+     *
+     * @param user the user to be deleted
+     */
     public void delete(User user);
+
 }
